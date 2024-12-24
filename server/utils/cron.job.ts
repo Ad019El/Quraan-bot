fbdf5957d94e095d6a17d346dff12344285176e1
Ad019El <a.dafi@esi-sba.dz>
2024-12-24 14:01:17 +0100

    test cron job in server

--- server/utils/cron.job.ts
+++ server/utils/cron.job.ts
@@ -3,3 +3,4 @@ import { broadcastVerse } from "../bot/commands/quran";
 import bot from "../bot";
 
 cron.schedule("0 8,12,20 * * *", () => broadcastVerse(bot));
+cron.schedule("30 14 * * *", () => broadcastVerse(bot));
