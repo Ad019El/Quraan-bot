8e33f97d9a7a7444ae70f7db18b803cb5fe92938
Ad019El <a.dafi@esi-sba.dz>
2024-12-25 22:32:28 +0100

    fix: update cron job schedule for broadcasting verses

--- server/utils/cron.job.ts
+++ server/utils/cron.job.ts
@@ -2,5 +2,5 @@ import cron from "node-cron";
 import { broadcastVerse } from "../bot/commands/quran";
 import bot from "../bot";
 
-cron.schedule("0 8,12,20 * * *", () => broadcastVerse(bot));
-cron.schedule("30 14 * * *", () => broadcastVerse(bot));
+cron.schedule("0 7,11,19 * * *", () => broadcastVerse(bot));
+// cron.schedule("30 14 * * *", () => broadcastVerse(bot));
